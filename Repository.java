d8818b2ee800ddac08e28d38ed786fb763d51399
Anamika Jain <94378420+ajain71@users.noreply.github.com>
2025-05-26 10:46:18 +0530

    Delete Repository.java

--- Repository.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-import jakarta.persistence.Id;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Repository extends JpaRepository<Employee,Integer> {
-}
